a278f62a6f68fe7d2f1ca98195dd81ce41e505cd
alexey vorovich <alexey.vorovich@gmail.com>
2020-12-29 23:09:07 -0500

    -file read OMG -json 1st cut -string

--- test01/test01.cpp
+++ test01/test01.cpp
@@ -2,6 +2,7 @@
 #include "test01.h"
 
 
+using namespace rapidjson;
 
 
 using namespace std;
@@ -121,12 +122,58 @@ private:
 
 	};
 
-private:
 #define IMAX_NAME 20
 	struct elm_t {
 		char name[IMAX_NAME + 1];
 		vector<int>* val_lst;
 	};
+
+
+public :
+	void get_json(string str) {
+		// 1. Parse a JSON string into DOM.
+
+		//const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+		auto json = new char[str.length() + 1];
+		strcpy(json, str.c_str());
+
+		Document d;
+		d.Parse(json);
+
+		// 2. Modify it by DOM.
+		//Value& s = d["stars"];
+		//s.SetInt(s.GetInt() + 1);
+
+		// 3. Stringify the DOM
+		StringBuffer buffer;
+		Writer<StringBuffer> writer(buffer);
+		d.Accept(writer);
+
+		// Output {"project":"rapidjson","stars":11}
+		std::cout << buffer.GetString() << std::endl;
+	
+	}
+	//to do relatvie file 
+	//todo iterate via json array
+	string  get_file() {
+		ifstream file1;
+		string line1;
+		string res;
+		string filename1 = "C:\\Users\\alexe\\source\\repos\\pgmabv99\\cpp_cmake2\\test01\\conf.json";
+		file1.open(filename1, ios::in);
+		if (file1.is_open()) {
+			while (getline(file1, line1)) {
+				cout << line1 << endl;
+				res = res + line1;
+			};
+			file1.close();
+		}
+		else {
+			cout << "err reading file" <<endl;
+		};
+		return res;
+	
+	}
 public:
 	void template_test() {
 		auto i_lst = new vector<int>{ 0 ,2};
@@ -204,7 +251,10 @@ int main()
 		//ptest1->print_v(vlist);
 
 	test1* ptest1 = new test1();
-	ptest1->triplets_test();
-	ptest1->template_test();
+	//ptest1->triplets_test();
+	
+	auto str=ptest1->get_file();
+	ptest1->get_json(str);
+	//ptest1->template_test();
 	return 0;
 }
--- test01/test01.h
+++ test01/test01.h
@@ -2,9 +2,15 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <list>
-#include <cstring>
+#include <cstring>  
+#include <string>
+#include "C:\Users\alexe\source\repos\rapidjson\include\rapidjson\document.h"
+#include "C:\Users\alexe\source\repos\rapidjson\include\rapidjson\writer.h"
+#include "C:\Users\alexe\source\repos\rapidjson\include\rapidjson\stringbuffer.h"
+
 #pragma warning (disable:4996)
 
 // TODO: Reference additional headers your program requires here.
